a7988a819200f7ed3a20c8127386c548106236ed
Vlad <vlad.a.borshch@gmail.com>
2024-07-16 16:17:55 +0300

    Made a real thumbnails for admin panel

--- Social Website/bookmarks/images/admin.py
+++ Social Website/bookmarks/images/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from easy_thumbnails.files import get_thumbnailer
+
 from .models import Image
 
 
@@ -11,7 +12,15 @@ class ImageAdmin(admin.ModelAdmin):
     readonly_fields = ['preview']
 
     def thumbnail(self, obj):
-        return format_html('<img src="{}" width="50" height="50" />', obj.image.url)
+        if obj.image:
+            thumbnailer = get_thumbnailer(obj.image)
+            try:
+                thumbnail = thumbnailer.get_thumbnail({'size': (50, 50)})
+                return mark_safe(f'<img src="{thumbnail.url}" width="50" height="50" />')
+            except Exception as e:
+                return f'Error generating thumbnail: {str(e)}'
+        else:
+            return 'No image available'
 
-    def preview(self,obj):
+    def preview(self, obj):
         return mark_safe(f'<img src="{obj.image.url}">')
